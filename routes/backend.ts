347f708f193a37f361678913aff1117a47dc469f
Login1990 <79404334+Login1990@users.noreply.github.com>
2023-12-20 19:01:40 +0200

    Implemented task 1

--- routes/backend.ts
+++ routes/backend.ts
@@ -1,8 +1,41 @@
 import express, {Express, NextFunction, Router, Request, Response} from "express"
+import bycript, {genSalt, hash} from "bcryptjs"
+
 const router = Router()
+
+type User = {
+    id: number,
+    username: string,
+    password: string,
+}
+
+let users: {[key: string]: User}= {}
+
+let user_id : number = 0
+
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.send("Hello from TS!")
 });
+
+router.post("/api/user/register", (req: Request, res: Response, next: NextFunction) => {
+    if(users.hasOwnProperty(req.body.username)){
+        return res.status(400).send("Username already used")
+    } else {
+        bycript.genSalt(10, (err, salt) => {
+            bycript.hash(req.body.password, salt, (err, hash) => {
+                if (err) throw err;
+                let user: User = {
+                    id: user_id,
+                    username: req.body.username,
+                    password: hash
+                }
+                users[req.body.username] = user
+                user_id += 1
+                return res.send(user)
+            })
+        })
+    }
+})
 
 export default router
